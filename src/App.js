7c271b4d337e18b7f526464eb780926142a26b42
Jon Sawyers <tg9569pb@gmail.com>
2021-08-11 12:57:18 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+
+const [photo, setPhoto] = useState([])
+
+useEffect(() => {
+  axios.get(``)
+})
 
 function App() {
   return (
