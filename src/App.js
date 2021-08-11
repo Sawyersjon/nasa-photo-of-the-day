506db8f45a5d20378fb8c91262ea99dcde4adc3b
Jon Sawyers <tg9569pb@gmail.com>
2021-08-11 13:08:08 -0500

    made connection with api key and recieved an object from the api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
 
-const [photo, setPhoto] = useState([])
 
-useEffect(() => {
-  axios.get(``)
-})
+
+
 
 function App() {
+
+  const [photo, setPhoto] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=GJMMEWVSfEdzwBB2HgvjlAXRGThD9aBfQ9okkKDO`)
+      .then(res => {
+        console.log(res)
+      })
+  })
+
   return (
     <div className="App">
       <p>
